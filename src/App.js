a1d7544f28a1ca1d2e71fdd0c11d1f76b7ac66aa
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-05-14 13:48:53 +0100

    header and nav components completed with the css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,14 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+
 function App() {
+  return (
+    <div>
+      <header class="header">
+        <Header />
+      </header>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,17 @@
+import './Header.css'
+import React from "react";
+import Nav from '../Navbar/Nav'
+
+const Header = () => {
+  return(
+      <div>
+          <h1>
+            <a href="">My Portfolio</a> 
+        </h1>
+        <Nav />
+        
+      </div>
+  )
+};
+
+export default Header;
