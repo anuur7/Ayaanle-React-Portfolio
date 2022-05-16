df5feedb62357e8240ecb9aa3179032f305792a2
Ayaanle Nur <Ayaanle-n@hotmail.com>
2022-05-16 19:09:59 +0100

    created intro component but image not loading

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import "./App.css";
 import Header from "./components/Header/Header";
+import Intro from './components/Intro/Intro'
 
 function App() {
   return (
     <div>
-      <header class="header">
+      <header className="header">
         <Header />
       </header>
+
+      <section className="picture-intro">
+        <Intro />
+      </section>
     </div>
   );
 }
---
+++ src/components/Intro/Intro.js
@@ -0,0 +1,16 @@
+import './Intro.css'
+import React from "react";
+
+const Intro = () => {
+  return (
+    <div>
+      <img className="my-picture" src='images/suit.jpeg' alt="me in a suit"></img>
+      <div className="intro">
+        <h1>Hi, I'm Ayaanle Nur</h1>
+        <h2>An aspiring software developer</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Intro;
